5a3dac5aac58ff813c76272de795376c216fce79
BugHunter-X <askzenai@gmail.com>
2024-12-26 18:30:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  try {
+    if (req.url === '/error') {
+      throw new Error('Something went wrong!');
+    }
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
+  } catch (err) {
+    console.error('Request handler error:', err);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
+  }
+});
+
+server.on('error', err => {
+  console.error('Server error:', err);
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
